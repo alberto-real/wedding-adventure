5a0096f0b159b8b1c2f73234cb75974225bb76e0
Alberto Real <albertorealestepa@gmail.com>
2026-03-19 15:40:53 +0100

    [Front] feat: last minut geocaching changes

--- apps/front/src/app/features/geocaching/geocaching.ts
+++ apps/front/src/app/features/geocaching/geocaching.ts
@@ -1,12 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-geocaching',
-  standalone: true,
-  imports: [CommonModule, RouterModule, TranslateModule],
+  imports: [RouterModule, TranslateModule],
   templateUrl: './geocaching.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
